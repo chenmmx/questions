52471104de5ec81f4b3a28dc8b0d3e5d0e4647e4
chenmmx <1107204891@qq.com>
2020-07-03 11:35:41 +0800

    feat: change method to new function

--- question2/js/index.js
+++ question2/js/index.js
@@ -6,69 +6,88 @@
     this ||
     {};
 
-  var lazyloadImages = {
-    data: {
-      oimgs: null,
+  var util = {
+    extend: function (target) {
+      for (var i = 1, len = arguments.length; i < len; i++) {
+        for (var prop in arguments[i]) {
+          if (arguments[i].hasOwnProperty(prop)) {
+            target[prop] = arguments[i][prop];
+          }
+        }
+      }
+
+      return target;
     },
-    /* 初始化
-     * @method init
-     * @return void
-     */
-    init: function () {
-      this.data.oimgs = document.querySelectorAll("img");
-      this.lazyLoad(this.data.oimgs);
-    },
-    /* 绑定dom事件
-     * @method bindEvent
-     * @return void
-     */
-    bindEvent: function () {},
+  };
+
+  function LazyloadImages(opts) {
+    this.opts = util.extend({}, this.constructor.defaultOpts, opts);
+    this.init(opts.node);
+  }
+
+  LazyloadImages.defaultOpts = {
+    node: "",
+  };
+
+  var proto = LazyloadImages.prototype;
+  /* 初始化
+   * @method init
+   * @return void
+   */
+  proto.init = function (node) {
+    this.oimgs = document.querySelectorAll(node);
+    this.lazyLoad(this.oimgs);
+  };
+  /* 绑定dom事件
+   * @method bindEvent
+   * @return void
+   */
+  (proto.bindEvent = function () {}),
     /* 获取页面顶部
      * @method getTop
      * @return void
      */
-    getTop: function (e) {
+    (proto.getTop = function (e) {
       var T = e.offsetTop;
       while ((e = e.offsetParent)) {
         T += e.offsetTop;
       }
       return T;
-    },
-    /* 懒加载图片
-     * @method lazyLoad
-     * @param {Dom} imgs 懒加载img dom
-     * @return void
-     */
-    lazyLoad: function (imgs) {
-      var H = document.documentElement.clientHeight; //获取可视区域高度
-      var S = document.documentElement.scrollTop || document.body.scrollTop;
-      for (var i = 0; i < imgs.length; i++) {
-        if (
-          H + S > this.getTop(imgs[i]) &&
-          this.getTop(imgs[i]) > S - imgs[i].height
-        ) {
-          if (imgs[i].src) {
-          } else {
-            imgs[i].src = imgs[i].getAttribute("data-src");
-          }
+    });
+  /* 懒加载图片
+   * @method lazyLoad
+   * @param {Dom} imgs 懒加载img dom
+   * @return void
+   */
+  proto.lazyLoad = function (imgs) {
+    var H = document.documentElement.clientHeight; //获取可视区域高度
+    var S = document.documentElement.scrollTop || document.body.scrollTop;
+    for (var i = 0; i < imgs.length; i++) {
+      if (
+        H + S > this.getTop(imgs[i]) &&
+        this.getTop(imgs[i]) > S - imgs[i].height
+      ) {
+        if (imgs[i].src) {
+        } else {
+          imgs[i].src = imgs[i].getAttribute("data-src");
         }
       }
-    },
+    }
   };
 
   // 判断当前环境是否支持模块导出
   if (typeof exports != "undefined" && !exports.nodeType) {
     if (typeof module != "undefined" && !module.nodeType && module.exports) {
-      exports = module.exports = lazyloadImages;
+      exports = module.exports = LazyloadImages;
     }
-    exports.lazyloadImages = lazyloadImages;
+    exports.LazyloadImages = LazyloadImages;
   } else {
-    root.lazyloadImages = lazyloadImages;
+    root.LazyloadImages = LazyloadImages;
   }
 })();
 
-  
-  window.onload = window.onscroll = function () {
-      lazyloadImages.init();
-  }
-  
+window.onload = window.onscroll = function () {
+  new LazyloadImages({
+    node: 'img'
+  })
+};
